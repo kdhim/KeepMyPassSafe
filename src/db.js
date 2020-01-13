b61d116cd3af02ff375150d28d187e53aa5b49af
Kevin Dhimitri <wonopon12@aol.com>
2020-01-12 19:46:50 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -76,13 +76,14 @@ let dbconf;
 
 dbconf = 'mongodb://admin:6iJHHadmbbUvELjU@172.30.37.82:27017/sampledb';
 
-/*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    dbconf = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+	console.log(process.env.OPENSHIFT_MONGODB_DB_URL);
+    /*dbconf = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
-  };*/
+    process.env.OPENSHIFT_APP_NAME;*/
+  };
 
 mongoose.connect(dbconf);
 
